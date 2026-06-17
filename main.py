380d5b80c71f37b9aef1315e26e814ff9fa0db72
Aayushmore11 <aayushmore6011@gmail.com>
2026-06-17 15:38:57 +0530

    Added post request

--- main.py
+++ main.py
@@ -1,12 +1,69 @@
-from fastapi import FastAPI, Path ,HTTPException, Query
-import json
+from fastapi import FastAPI, Path ,HTTPException, Query  
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel  , Field , computed_field
+from typing import Annotated , Literal 
+import json 
 
 app = FastAPI()
+
+class Patient(BaseModel):   #mention all classes in the json file 
+     
+    id:Annotated[str,Field(...,description='Id of the patient',examples=['P001'])]
+    name:Annotated[str,Field(...,description='Name of the patient')]
+    city:Annotated[str,Field(...,description='city where the patient is living')]
+    age:Annotated[int,Field(...,gt=0, lt=120, description='age pf the patient')]
+    gender:Annotated[Literal['male','female','others'], Field(...,description='gender of the patient')]
+    height:Annotated[float, Field(...,gt=0,description='height of the patient in mtrs')]
+    weight:Annotated[float,Field(...,gt=0 ,lt=150,description='weight of the patient in kgs ')]
+    
+    @computed_field
+    @property 
+    def bmi(self)-> float:
+        bmi=round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:   #verdict is taking heklp from bmi which is itself a omputed field A computed field is a value that is:not stored directly,calculated dynamically from other fields in the model
+
+        if self.bmi<18.5:
+            return 'underweight'
+        elif self.bmi<25:
+            return 'normal'
+        elif self.bmi<30:
+            return 'overweight'
+        else:
+            return 'obese'
 
 def load_data():
     with open('patients.json', 'r') as file:
         data = json.load(file)
     return data
+
+def save_data(data):
+    with open('patients.json', 'w')as file:
+        json.dump(data,file)
+    
+
+
+    
+    """when pydantic creates the verdict it creates or triggers this code
+    which itself has a computed field in it and so 
+    there it goes to the
+      computed fiekd of bmi and tae infomation from there and puts to value here to check on this computed field to check its value
+    basicallly one field is dependent on the other field 
+    """
+
+
+
+
+
+
+
+
+
+
+
 
 @app.get("/")
 def hello():
@@ -46,3 +103,28 @@ order: str=Query('asc', description = 'order of sorting, asc or dsc',)):
     sorted_data = sorted(data.values(), key=lambda x: x[sort_by],reverse=(order=='dsc'))
     return sorted_data
 
+@app.post('/create')
+def create_patient(patient:Patient): #patient here is pydantic object
+    
+     #:patient data is coming which is coming in request model we are sending it to the pydantic model and the pydantic model will just check if it is in a righ tformat or not 
+    '''if the data is in the right format then only we lll move forwad or else we will face some error 
+    we dont create or send the reqyest manuaally to the pydantic modelbehind the scenes fastapi is doing ll this work
+     '''
+     
+     #load existing data
+    data=load_data()   #the existing dataa is python dict and the patient is pydantic objject
+
+
+     # check if the patients already exists 
+    if patient.id in data:
+        raise HTTPException(status_code=400,detail='patient already exists')
+    
+
+     # if patients is not there rthen we willl
+     # add new patient totthe datasbase 
+    data[patient.id] = patient.model_dump(exclude=['id'])
+     
+#save into the  json file 
+    save_data(data)
+
+    return JSONResponse(status_code=201,content={'message':'patient created successfully'})
